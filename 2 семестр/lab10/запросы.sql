fba39764785272e9e9d435138984b30f0359c35f
evgeniy <git_evg_ost@internet.ru>
2025-05-20 21:12:35 +0300

    Change запросы.sql: add questions 4-8

--- 2 семестр/lab10/запросы.sql
+++ 2 семестр/lab10/запросы.sql
@@ -25,3 +25,49 @@ SELECT DISTINCT sg."КодГруппы",
 FROM (Faculty.sgroup sg JOIN Faculty.lesson l ON sg."КодГруппы" = l."Группа")
 	JOIN Faculty.subject s ON l."Дисциплина" = s."КодДисциплины"
 ORDER BY sg."КодГруппы";
+
+--Задание 4
+SELECT sg."КодГруппы",
+	COUNT(s."НомерЗачетнойКнижки") AS "Количество студентов"
+FROM Faculty.student s
+JOIN Faculty.sgroup sg ON s."Группа" = sg."КодГруппы"
+GROUP BY sg."КодГруппы"
+ORDER BY sg."КодГруппы";
+
+--Задание 5
+SELECT d."Название кафедры" AS "Кафедра",
+	COUNT(t."КодПреподавателя") AS "Количество преподавателей"
+FROM Faculty.teacher t
+JOIN faculty.teacher_department td ON t."КодПреподавателя" = td."Преподаватель"
+JOIN faculty.department d ON td."Кафедра" = d."Код кафедры"
+GROUP BY d."Название кафедры"
+HAVING COUNT(t."КодПреподавателя") < 3;
+
+--Задание 6
+SELECT t."Фамилия" || ' ' || LEFT(t."Имя", 1) || '.' || LEFT(t."Отчество", 1) AS "ФИО",
+	COUNT(td."Кафедра") AS "Количество кафедр"
+FROM faculty.teacher t
+JOIN faculty.teacher_department td ON t."КодПреподавателя" = td."Преподаватель"
+GROUP BY "ФИО"
+HAVING COUNT(td."Кафедра") > 1;
+
+--Задание 7
+SELECT t."Фамилия" || ' ' || LEFT(t."Имя", 1) || '.' || LEFT(t."Отчество", 1) AS "ФИО",
+	sg."КодГруппы",
+	s."Название",
+	l."ВидЗанятия"
+FROM faculty.lesson l
+JOIN faculty.teacher t ON l."Преподаватель" = t."КодПреподавателя"
+JOIN faculty.sgroup sg ON l."Группа" = sg."КодГруппы"
+JOIN faculty.subject s ON l."Дисциплина" = s."КодДисциплины"
+WHERE NOT s."КоличествоЛекционныхЧасов" = 0
+ORDER BY s."Название";
+
+--Задание 8
+SELECT t."Фамилия" || ' ' || LEFT(t."Имя", 1) || '.' || LEFT(t."Отчество", 1) AS "ФИО",
+	COUNT(DISTINCT s."НомерЗачетнойКнижки") AS "Количество студентов"
+FROM faculty.teacher t
+JOIN faculty.lesson l ON t."КодПреподавателя" = l."Преподаватель"
+JOIN faculty.student s ON l."Группа" = s."Группа"
+GROUP BY t."КодПреподавателя"
+HAVING COUNT(DISTINCT s."НомерЗачетнойКнижки") > 3;
